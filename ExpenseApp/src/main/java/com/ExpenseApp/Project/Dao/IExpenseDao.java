3245c5109134e20ba0981fa7e7e92b4cd984cda5
Nilesh Solankar <50072002+Nileshs1195@users.noreply.github.com>
2019-11-24 23:49:06 -0800

    Project Completed
    Made necessary Changes.

---
+++ ExpenseApp/src/main/java/com/ExpenseApp/Project/Dao/IExpenseDao.java
@@ -0,0 +1,19 @@
+package com.ExpenseApp.Project.Dao;
+
+import java.util.List;
+
+import com.ExpenseApp.Project.pojo.Expense;
+
+public interface IExpenseDao
+{
+	String addExpense(Expense expense);
+
+	Expense getExpense(int expenseid);
+
+	List<Expense> getExpenseList(Expense expense);
+
+	List<Object> getMonthlyExpense();
+
+	List<Object> getUsersMonthlyExpense(Integer id);
+	
+}
